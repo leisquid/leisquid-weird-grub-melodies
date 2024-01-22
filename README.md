d150fc0abb14b79e254bc04cb88788eac9406a6f
Li Tianyi <li.tianyi@canon-systemsoft.com.cn>
2024-01-22 16:12:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# leisquid-weird-grub-melodies
+
+Just some weird grub melodies for playing before entering to GRUB, written by me.
+
+leisquid
+
+24.1.22
---
+++ melodies.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+menuentry "Eat Coins" {
+    play 600 988 1 1319 4
+}
+
+menuentry "All Correct" {
+    play 810 1319 1 0 1 1568 1 0 1 2637 1 0 1 2093 1 0 1 2349 1 0 1 3136 1
+}
+
+menuentry "CRH2C Departing" {
+    play 160 1 1 311 1 466 1 784 1 466 1 622 1 466 1 698 1 932 3
+}
